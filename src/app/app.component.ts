c09907a4a2c868271c8138dc4d028e6ef05c3754
simpleappdesigner <simpleappdesigner@gmail.com>
2022-09-29 19:34:14 -0400

    boiler plate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stays';
+
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 600,
+    navText: ['&#8249', '&#8250;'],
+   // navText: ['<<', '>>'],
+   //navText: ["<i class='fa fa-chevron-right'></i>","<i class='fa fa-chevron-left'></i>"],
+    responsive: {
+      0: {
+        items: 1 
+      },
+      400: {
+        items: 2
+      },
+      760: {
+        items: 3
+      },
+      1000: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+//import { NgModule } from '@angular/core';
+//import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//carousel-o
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CarouselModule } from 'ngx-owl-carousel-o';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule, // carousel import
+    CarouselModule // carousel import
   ],
   providers: [],
   bootstrap: [AppComponent]
